15d6e894ce236be3a4dda0591564541ee3db7463
Chidera <nwankwodera321@gmail.com>
2025-12-12 23:21:09 +0100

    Ready to deploy part 2

--- library/settings.py
+++ library/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-i9sb!dodjdq-moij-_605jwfuiu+p5^f1yl2i66_v5^awkxr8m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['campus.onrender.com']
 
 
 # Application definition
@@ -74,10 +76,11 @@ WSGI_APPLICATION = 'library.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
@@ -117,12 +120,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-'''STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-] 
-
 STATIC_ROOT = BASE_DIR / "staticfiles"
-'''
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
