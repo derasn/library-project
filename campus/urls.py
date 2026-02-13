bf043bfa4bc2a215f785909114421a57945085c4
Chidera <nwankwodera321@gmail.com>
2026-02-13 20:32:32 +0100

    register-add, added header back on mobile, optimizing recently added

--- campus/urls.py
+++ campus/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('upload/', views.upload_material, name='upload'),
-    path('register/', views.register_course, name='register'),
+    path('add/', views.register_course, name='register'),
     path('subject/<int:subject_id>/', views.subject_courses, name='subject_courses'),
     path('level/<str:level>/', views.level_courses, name='level_courses'),
     path('course/<int:course_id>/', views.course_details, name='course_details'),
